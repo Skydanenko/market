f3cbf43d3723cf22c95bd47878780ca1f2d06ff1
Skydanenko <andriiskydanenko@Noutbuk-Andrii.local>
2023-09-25 16:48:10 +0300

    created vue project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <nav class="nav">
+      <router-link class="nav__link" :to="{ name: 'Home' }">Home</router-link> |
+      <router-link class="nav__link" :to="{ name: 'About' }">About</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import "styles/common";
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+  background-color: #000;
+  min-height: 100vh;
+}
+
+.nav {
+  padding: 30px;
+
+  &__link {
+    font-weight: bold;
+    color: #777;
+    text-decoration: none;
+
+    &:hover {
+      color: #ad9a0a;
+    }
+
+    &.router-link-exact-active {
+      color: #ffe81e;
+    }
+  }
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <p>some text</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class About extends Vue {}
+</script>
